f6b2abb1f84e93a101068d763cf770e2edca0a4c
asp <asp@aptech.com>
2026-06-16 12:35:36 +0500

    my file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 import './App.css'
-
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
 function App() {
  
-
+ useEffect(() => {    ReactGA.send({ hitType: "pageview", page: window.location.pathname });  }, []);
   return (
    <div>
     <h1>Welcome to online Course Website</h1>
@@ -11,5 +12,4 @@ function App() {
    </div>
   )
 }
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+//npm install react-ga4
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot , ReactDOM} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import React from "react";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-HJJBSRCKXM");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>)
+
